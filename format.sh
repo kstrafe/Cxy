ae32dba9d3d49d6f5706967712b7de1745c7529a
kstraver <kstraver@cisco.com>
2015-07-29 20:02:56 +0200

    Remove the formatter script

--- format.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-in_file=$(find . | grep -P "\.py|\.cpp|\.hpp")
-for in_file in $in_file; do
-	sed 's/^\(\t*\)\(  \)/\1\t/g' "$in_file" > "${in_file}xxx"
-	mv "${in_file}xxx" "$in_file"
-done
