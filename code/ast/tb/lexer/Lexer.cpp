85304fc40ef0b98b1dd3348e3ec9e347d58ccdcc
kstraver <kstraver@cisco.com>
2015-08-08 22:05:22 +0200

    Remove the makefile. Add comment on upcoming features.

--- code/ast/tb/lexer/Lexer.cpp
+++ code/ast/tb/lexer/Lexer.cpp
@@ -7,138 +7,138 @@ it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-ULCRI is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+	ULCRI is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with ULCRI.  If not, see <http://www.gnu.org/licenses/>.
-*/
-#include "Lexer.hpp"
-#include "dependency/KeywordMatcher.hpp"
-#include "dependency/SymbolMatcher.hpp"
-#include "protocols/Token.hpp"
+	You should have received a copy of the GNU General Public License
+	along with ULCRI.  If not, see <http://www.gnu.org/licenses/>.
+	*/
+	#include "Lexer.hpp"
+	#include "dependency/KeywordMatcher.hpp"
+	#include "dependency/SymbolMatcher.hpp"
+	#include "protocols/Token.hpp"
 
-#include <algorithm>
-#include <cassert>
-#include <iostream>
-#include <limits>
-#include <vector>
+	#include <algorithm>
+	#include <cassert>
+	#include <iostream>
+	#include <limits>
+	#include <vector>
 
 
-namespace tul { namespace lexer {
+	namespace tul { namespace lexer {
 
-bool Lexer::insertCharacter(char character_)
-{
-	comment_buffer.putCharInto(character_);
-	uint8_t take_characters = comment_ignorer.putOnStack(character_);
-	for (; take_characters > 0; --take_characters)
-		if (insertCharacterAfterComments(comment_buffer.getCharFrom(take_characters)))
-			continue;
-		else
-			return false;
-	return true;
-}
-
-bool Lexer::insertCharacterAfterComments(char character_)
-{
-	protocols::EntryType type_of_character = typify(character_);
-	protocols::Action action_to_perform = action_generator.computeAction(type_of_character);
+	bool Lexer::insertCharacter(char character_)
 	{
-		std::size_t amount_of_new_tokens = token_generator.consumeCharacter(character_, action_to_perform);
-		if (amount_of_new_tokens == std::numeric_limits<std::size_t>::max())
-		{
-			return false;
-		}
-		// A new token is ready to be identified
-		for
-		(
-			std::size_t new_token = getTokenStack().size() - amount_of_new_tokens
-			; new_token < getTokenStack().size();
-			++new_token
-		)
-		{
-			protocols::Token &curr_token = getTokenStack()[new_token];
-			identifyToken(curr_token);
-
-			{
-				protocols::Token potential_next {0, 0, protocols::EntryType::OTHER_SYMBOL, protocols::TokenType::UNIDENTIFIED, ""};
-				while
-				(
-					curr_token.token_type == protocols::TokenType::UNIDENTIFIED
-					&& curr_token.entry_type == protocols::EntryType::OTHER_SYMBOL
-				)
-				{
-					if (curr_token.accompanying_lexeme.empty())
-					{
-						return false;
-					}
-					// Take the last character off...
-					char last = curr_token.accompanying_lexeme.back();
-					curr_token.accompanying_lexeme.erase(curr_token.accompanying_lexeme.size() - 1);
-					potential_next.accompanying_lexeme.insert(potential_next.accompanying_lexeme.begin(), last);
-					identifyToken(curr_token);
-				}
-				if (potential_next.accompanying_lexeme.empty() == false)
-				{
-					getTokenStack().insert(getTokenStack().begin() + new_token + 1, potential_next);
-				}
-			}
-			getTokenStack()[new_token].column_number = position_counter.getCurrentColumnNumber();
-			getTokenStack()[new_token].line_number = position_counter.getCurrentLineNumber();
-		}
+		comment_buffer.putCharInto(character_);
+		uint8_t take_characters = comment_ignorer.putOnStack(character_);
+		for (; take_characters > 0; --take_characters)
+			if (insertCharacterAfterComments(comment_buffer.getCharFrom(take_characters)))
+				continue;
+			else
+				return false;
+		return true;
 	}
-	position_counter.countCharacter(character_);
-	return true;
-}
 
-std::vector<protocols::Token> &Lexer::getTokenStack()
-{
-	return token_generator.getTokenStack();
-}
+	bool Lexer::insertCharacterAfterComments(char character_)
+	{
+		protocols::EntryType type_of_character = typify(character_);
+		protocols::Action action_to_perform = action_generator.computeAction(type_of_character);
+		{
+			std::size_t amount_of_new_tokens = token_generator.consumeCharacter(character_, action_to_perform);
+			if (amount_of_new_tokens == std::numeric_limits<std::size_t>::max())
+			{
+				return false;
+			}
+			// A new token is ready to be identified
+			for
+			(
+				std::size_t new_token = getTokenStack().size() - amount_of_new_tokens
+				; new_token < getTokenStack().size();
+				++new_token
+			)
+			{
+				protocols::Token &curr_token = getTokenStack()[new_token];
+				identifyToken(curr_token);
 
-protocols::TokenType Lexer::getKeyword(const std::string &test_lexeme) const
-{
-	return dependency::KeywordMatcher::getKeyword(test_lexeme);
-}
+				{
+					protocols::Token potential_next {0, 0, protocols::EntryType::OTHER_SYMBOL, protocols::TokenType::UNIDENTIFIED, ""};
+					while
+					(
+						curr_token.token_type == protocols::TokenType::UNIDENTIFIED
+						&& curr_token.entry_type == protocols::EntryType::OTHER_SYMBOL
+					)
+					{
+						if (curr_token.accompanying_lexeme.empty())
+						{
+							return false;
+						}
+						// Take the last character off...
+						char last = curr_token.accompanying_lexeme.back();
+						curr_token.accompanying_lexeme.erase(curr_token.accompanying_lexeme.size() - 1);
+						potential_next.accompanying_lexeme.insert(potential_next.accompanying_lexeme.begin(), last);
+						identifyToken(curr_token);
+					}
+					if (potential_next.accompanying_lexeme.empty() == false)
+					{
+						getTokenStack().insert(getTokenStack().begin() + new_token + 1, potential_next);
+					}
+				}
+				getTokenStack()[new_token].column_number = position_counter.getCurrentColumnNumber();
+				getTokenStack()[new_token].line_number = position_counter.getCurrentLineNumber();
+			}
+		}
+		position_counter.countCharacter(character_);
+		return true;
+	}
+
+	std::vector<protocols::Token> &Lexer::getTokenStack()
+	{
+		return token_generator.getTokenStack();
+	}
+
+	protocols::TokenType Lexer::getKeyword(const std::string &test_lexeme) const
+	{
+		return dependency::KeywordMatcher::getKeyword(test_lexeme);
+	}
 
 
-void Lexer::identifyToken(protocols::Token &input_token)
-{
-	using namespace protocols;
-	const std::string &a_lexeme = input_token.accompanying_lexeme;
+	void Lexer::identifyToken(protocols::Token &input_token)
+	{
+		using namespace protocols;
+		const std::string &a_lexeme = input_token.accompanying_lexeme;
 
-	std::size_t left_underscores = 0, right_underscores = 0;
-	for (char lex_char : a_lexeme)
-		if (lex_char == '_')
-			++left_underscores;
-		else
-			break;
+		std::size_t left_underscores = 0, right_underscores = 0;
+		for (char lex_char : a_lexeme)
+			if (lex_char == '_')
+				++left_underscores;
+			else
+				break;
 
-	for (std::size_t back_it = a_lexeme.size() - 1; back_it > 0; --back_it)
-		if (a_lexeme.at(back_it) == '_')
-			++right_underscores;
-		else
-			break;
+		for (std::size_t back_it = a_lexeme.size() - 1; back_it > 0; --back_it)
+			if (a_lexeme.at(back_it) == '_')
+				++right_underscores;
+			else
+				break;
 
-	auto has_trailing_underscores = [&]() -> bool { return left_underscores + right_underscores > 0; };
-	// Ensure there are at least +1 characters, the string can NOT be ____, or ____, or __, or _, _a__ is allowed
-	assert(a_lexeme.size() > left_underscores + right_underscores);
-	// Strip the trailing underscores
-	std::string substitution_lexeme = a_lexeme.substr(left_underscores, a_lexeme.size() - right_underscores - left_underscores);
-	protocols::Token dummy_token = input_token;
-	dummy_token.accompanying_lexeme = substitution_lexeme;
-	const std::string backup_string = a_lexeme;
-	identifyTokenAfterStrippingUnderscores(dummy_token);
-	input_token = dummy_token;
-	input_token.accompanying_lexeme = backup_string;
+		auto has_trailing_underscores = [&]() -> bool { return left_underscores + right_underscores > 0; };
+		// Ensure there are at least +1 characters, the string can NOT be ____, or ____, or __, or _, _a__ is allowed
+		assert(a_lexeme.size() > left_underscores + right_underscores);
+		// Strip the trailing underscores
+		std::string substitution_lexeme = a_lexeme.substr(left_underscores, a_lexeme.size() - right_underscores - left_underscores);
+		protocols::Token dummy_token = input_token;
+		dummy_token.accompanying_lexeme = substitution_lexeme;
+		const std::string backup_string = a_lexeme;
+		identifyTokenAfterStrippingUnderscores(dummy_token);
+		input_token = dummy_token;
+		input_token.accompanying_lexeme = backup_string;
 
-	if (has_trailing_underscores())
-		if (input_token.token_type == TokenType::IDENTIFIER_PACKAGE)
-			input_token.token_type = TokenType::IDENTIFIER_VARIABLE;
+		if (has_trailing_underscores())
+			if (input_token.token_type == TokenType::IDENTIFIER_PACKAGE)
+				input_token.token_type = TokenType::IDENTIFIER_VARIABLE;
 
-	/*
+		/*
 	switch (left_underscores)
 	{
 		default: break;
@@ -245,6 +245,7 @@ void Lexer::identifyTokenAfterStrippingUnderscores(protocols::Token &input_token
 		{
 			input_token.token_type = getKeyword(input_token.accompanying_lexeme);
 		}
+		// Fold this with identifier variable.
 		else if (is_package_identifier())
 		{
 			input_token.token_type = TokenType::IDENTIFIER_PACKAGE;
