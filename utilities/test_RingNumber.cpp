df80f6c4bc8ab03ee01ede72a9f3c9e9077d0358
BourgondAries <macocio@gmail.com>
2015-10-10 11:01:45 +0200

    Fix RingNumber not including the maximum value.
    Add a test to ringnumber to test common edge cases and
    general use. RingNumber should be stable now.
    
    It is only defined for the range [0, max]. Numbers below zero
    are omitted, although these could be added in the future. I see
    no need to complicate this binary counter. A simpler setup would
    be to use modulos to normalize the values, but this does not allow
    inclusive maximum counting.

---
+++ utilities/test_RingNumber.cpp
@@ -0,0 +1,62 @@
+// Copyright © 2015 Kevin Robert Stravers
+/*
+This file is part of Cxy Compiler Reference Implementation (Cxy CRI).
+
+Cxy CRI is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Cxy CRI is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Cxy CRI.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include "libraries/catch.hpp"
+#include "utilities/RingNumber.hpp"
+
+#include <limits>
+
+
+TEST_CASE("RingNumber must behave in a modulus way", "[test-RingNumber]")
+{
+	using namespace tul::utilities;
+	SECTION("RingNumber standard semantics")
+	{
+		RingNumber<uint32_t> ring;
+		REQUIRE(ring.getNumber() == 0);
+		REQUIRE(++ring == 1);
+		REQUIRE(++ring == 0);
+		REQUIRE(ring.getPrevious() == 1);
+		REQUIRE(--ring == 1);
+	}
+	SECTION("Integer overflow semantics")
+	{
+		RingNumber<int32_t> ring(std::numeric_limits<int32_t>::max());
+		int32_t manual_counter = 1;
+		#define CAZE() REQUIRE(manual_counter++ == ++ring);
+			CAZE()
+			CAZE()
+			CAZE()
+		#undef CAZE
+		ring.setNumber(std::numeric_limits<int32_t>::max() - 1);
+		REQUIRE(ring.getNumber() == std::numeric_limits<int32_t>::max() - 1);
+		REQUIRE(++ring == std::numeric_limits<int32_t>::max());
+		REQUIRE(++ring == 0);
+		REQUIRE(--ring == std::numeric_limits<int32_t>::max());
+	}
+	SECTION("0-9")
+	{
+		RingNumber<int8_t> ring(9);
+		int8_t manual_counter = 1;
+		#define CAZE() REQUIRE(manual_counter++ == ++ring);
+			for (int i = 0; i < 9; ++i)
+				CAZE()
+		#undef CAZE
+		REQUIRE(--ring == 8);
+		REQUIRE(++ring == 9);
+	}
+}
