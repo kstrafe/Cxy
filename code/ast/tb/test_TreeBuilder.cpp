c12db455f8a4d12ded375c65b2493f9dfc37af3b
BourgondAries <macocio@gmail.com>
2015-11-25 04:51:56 +0100

    Add another experimental test

--- code/ast/tb/test_TreeBuilder.cpp
+++ code/ast/tb/test_TreeBuilder.cpp
@@ -1124,10 +1124,14 @@ TEST_CASE("TreeBuilder must validate input", "[test-TreeBuilder]")
 				experimental
 					Class {a: x, b, c: d}, Other e: f = cool();
 				experimental
-					Class {a: b, b: a}, c = g(),
+					a: b, Class {a: b, b: a}, c = g(),
 						Ff d = sin(:3*5) + -5,
 						Muh x: e
 							= f();
+				experimental
+					a = g();
+				experimental
+					x: a, y: b, Zz z: c, w: d = f();
 			}
 		)");
 
