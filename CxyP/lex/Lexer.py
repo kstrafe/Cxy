323c81c9a134e1dc4d83812301f2cba8a86852f0
Kevin Robert Stravers <macocio@gmail.com>
2016-02-19 18:21:41 +0100

    Remove obnoxious files

--- CxyP/lex/Lexer.py
+++ CxyP/lex/Lexer.py
@@ -7,14 +7,7 @@ def __init__(self, entrytypes):
 	self._typifier = typify.Typifier(entrytypes)
 	self._akt = self.util.Enum.generate('N', 'E', 'P', 'PTG', 'TAPTG', 'TA', 'TAP', 'TRP', 'TR', 'TRPTG', 'TSP', 'TSPTG', 'TS')
 	akt = self._akt
-	table = [
-	# ALPHA_DIGIT_OR_UNDERSCORE GROUPING_SYMBOL QUOTE_SYMBOL OTHER_SYMBOL  UNKNOWN_CODE_POINT WHITESPACE
-		[[1, akt.P],              [0, akt.PTG],   [2, akt.N],  [4, akt.P],   [0, akt.E],        [0, akt.N]],  # 0
-		[[1, akt.P],              [0, akt.TAPTG], [2, akt.TA], [4, akt.TAP], [0, akt.E],        [0, akt.TA]], # 1
-		[[2, akt.P],              [2, akt.P],     [3, akt.N],  [2, akt.P],   [2, akt.P],        [2, akt.P]],  # 2
-		[[1, akt.TRP],            [0, akt.TRPTG], [2, akt.P],  [4, akt.TRP], [0, akt.E],        [0, akt.TR]], # 3
-		[[1, akt.TSP],            [0, akt.TSPTG], [2, akt.TS], [4, akt.P],   [0, akt.E],        [0, akt.TS]],  # 4
-	]
+	table = act.ActionTable.getTable(self._akt)
 	self._mealy = mealy.Mealy(table)
 	toktype = self.util.Enum.generate('UNIDENTIFIED')
 	self._cluster = act.Cluster(akt, entrytypes, toktype)
@@ -28,3 +21,4 @@ def lex(self, character):
 	a = self._cluster.consume(character, action)
 	for i in range(a):
 		print(self._cluster.pop(), "is a token")
+	# Next up: create value
